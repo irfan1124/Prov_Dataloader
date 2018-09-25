f73bc97c219997c24ad3be40f8b7523249c97224
online-irfan <irfanali@opspl.com>
2018-09-25 12:07:09 +0530

    added typecast to knex config

--- server/db/config/knexConfig.js
+++ server/db/config/knexConfig.js
@@ -6,7 +6,13 @@ module.exports = {
       host : process.env.DATABASE_HOST,
       user : process.env.DATABASE_USERNAME,
       password : process.env.DATABASE_PASSWORD,
-      database : process.env.DATABASE_NAME
+      database : process.env.DATABASE_NAME,
+      typeCast: (field, next) => {
+        if (field.type == 'BIT' && field.length == 1) {
+            return (field.string() == '1'); // 1 = true, 0 = false
+        } 
+        return next();
+      }
     },
     pool: { min: 0, max: 7 }
 };
